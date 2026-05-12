a74505abc1272e58ad16d219c36c1544ba46bce1
kiritoz05 <kiritoztk05@gmail.com>
2026-05-12 10:39:32 -0500

    Delete frontend directory

--- frontend/src/pages/api/state.js
+++
@@ -1,5 +0,0 @@
-if (!global._tks) global._tks = { conn:null, state:{connected:false,user:null,viewers:0,gifts:0,followers:0,messages:0,likes:0,diamonds:0}, events:[] }
-const G = global._tks
-export default function handler(req, res) {
-  res.json({ state: G.state, events: G.events })
-}
